a79e30eedda0656c78216d7b40a95f73afaa4304
Aaron Zhu <zghcivilization@163.com>
2020-07-01 15:11:00 +0800

    update Abstract Factory Pattern

--- src/main/java/com/DesignPattern/AbstractFactoryPattern/PCFactory.java
+++ src/main/java/com/DesignPattern/AbstractFactoryPattern/PCFactory.java
@@ -4,7 +4,21 @@ package com.DesignPattern.AbstractFactoryPattern;
  * 抽象工厂角色：PC工厂
  */
 public interface PCFactory {
+    /**
+     * 提供显示器产品
+     * @return
+     */
     Monitor getMonitor();
+
+    /**
+     * 提供鼠标产品
+     * @return
+     */
     Mouse getMouse();
+
+    /**
+     * 提供键盘产品
+     * @return
+     */
     Keyboard getKeyboard();
 }
