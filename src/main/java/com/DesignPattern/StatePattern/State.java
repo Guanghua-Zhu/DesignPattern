888dadbe64fec7888fc3daf21e02b92a1f4ae0b7
Aaron Zhu <zghcivilization@163.com>
2020-08-20 17:55:48 +0800

    update state pattern

--- src/main/java/com/DesignPattern/StatePattern/State.java
+++ src/main/java/com/DesignPattern/StatePattern/State.java
@@ -5,11 +5,27 @@ package com.DesignPattern.StatePattern;
  */
 public interface State {
 
+    /**
+     * 预订
+     * @param room
+     */
     void book(Room room);
 
+    /**
+     * 取消预订
+     * @param room
+     */
     void cancel(Room room);
 
+    /**
+     * 支付
+     * @param room
+     */
     void pay(Room room);
 
+    /**
+     * 退房
+     * @param room
+     */
     void checkout(Room room);
 }
